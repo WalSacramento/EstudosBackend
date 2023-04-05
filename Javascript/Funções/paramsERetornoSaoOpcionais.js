bb07871f7c178e1b06cf86e8c2eaa31d6574c972
Waldsson Sacramento <waldsson.dev@gmail.com>
2023-04-04 23:47:42 -0300

    Day 01

---
+++ Javascript/Funções/paramsERetornoSaoOpcionais.js
@@ -0,0 +1,15 @@
+function area(largura, altura) {
+  const area = largura * altura
+  if (area > 20) {
+    console.log(`Valor acima do permitido: ${area}m2`)
+  } else {
+    return area
+  }
+}
+
+
+console.log(area (5, 2))
+console.log(area (5))
+console.log(area ())
+console.log(area (5, 2, 3))
+console.log(area (11, 2))
