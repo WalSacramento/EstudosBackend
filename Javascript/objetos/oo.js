bc9c1a3469ff15b2c18f167b7e1de0c2c7cd7692
Waldsson Sacramento <waldsson.dev@gmail.com>
2023-04-07 20:15:13 -0300

    Day 04

---
+++ Javascript/objetos/oo.js
@@ -0,0 +1,23 @@
+//CÓDIGO NÃO EXECUTAVEL!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//ORIENTADA A OBJETOS
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    //....
+  },
+}
+
+objeto.processamento() // O FOCO PASSOU A SER O OBJETO!
+
+/*PRINCIPIOS IMPORTANTES
+1.ABSTRAÇÃO
+2.ENCAPSULAMENTO
+3. HERANÇA (PROTOTYPE)
+4.POLIMORFISMO
+*/
